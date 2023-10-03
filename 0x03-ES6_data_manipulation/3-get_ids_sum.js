9f2980a6cdbae3b97fd430f191a4e7000571cde2
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-10-03 17:44:06 +0100

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(getListStudents) {
-    return getListStudents.reduce((sum, obj) => sum + obj.id, 0);
+  return getListStudents.reduce((sum, obj) => sum + obj.id, 0);
 }
