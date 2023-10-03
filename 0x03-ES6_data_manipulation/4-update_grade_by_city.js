091af7a89f4eba1cd86e2eaa732d1486cf851458
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-10-03 23:01:24 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-param-reassign */
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-    const students = getListStudents;
-    const updatedStudents = students.map(student => {
-      if (student.location === city) {
-        const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
-        if (matchingGrade) {
-          student.grade = matchingGrade.grade;
-        } else {
-          student.grade = "N/A";
-        }
+  const students = getListStudents;
+  const updatedStudents = students.map((student) => {
+    if (student.location === city) {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (matchingGrade) {
+        student.grade = matchingGrade.grade;
+      } else {
+        student.grade = 'N/A';
       }
-      return student;
-    });
-  
-    return updatedStudents;
-  }
+    }
+    return student;
+  });
+
+  return updatedStudents;
+}
