e7ccaa8cf5a952470840a9df9664cdb15957708e
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-11-22 05:12:51 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-    const name = input.toString().trim();
+  const name = input.toString().trim();
 
-    if (name) {
-        process.stdout.write(`Your name is: ${name}\n`);
-    }
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
