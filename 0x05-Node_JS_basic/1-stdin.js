3cc44da4df2d98bb605aca430932b4de72c07c7b
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-11-22 05:03:37 +0100

    Create 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdin.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (input) => {
+    const name = input.toString().trim();
+
+    if (name) {
+        process.stdout.write(`Your name is: ${name}\n`);
+    }
+
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+});
