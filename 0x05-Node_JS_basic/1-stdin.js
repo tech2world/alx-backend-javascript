a4e004aca59a24f8220786525e6e59e665728ad3
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-11-26 16:07:49 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,20 @@
-process.stdin.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
+    const name = input.trim();
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
+    if (process.stdin.isTTY) {
+        process.stdout.write(`Your name is: ${name}\n`);
+        process.exit();
+    } else {
+        process.stdout.write(`Your name is: ${name}\nThis important software is now closing\n`);
+        process.exit();
+    }
+});
 
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+process.stdin.on('error', (err) => {
+    process.stderr.write(`Error: ${err}\n`);
+    process.exit(1);
 });
