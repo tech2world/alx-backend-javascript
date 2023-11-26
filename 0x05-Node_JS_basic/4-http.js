10f1133f161e9fd342267473d610e518c9d5b179
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-11-26 18:05:07 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const port = 12345;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, host, () => {});
